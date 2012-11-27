2f656795ff61095a5c7e240ff5cae993ca236beb
Matthieu Dien <matthieu@matthieu-Kubu.(none)>
2012-11-27 22:38:43 +0100

    les sockets python commencent tous doux

--- Client/python/connection/view.py
+++ Client/python/connection/view.py
@@ -17,10 +17,15 @@ class View(wx.BoxSizer):
     def InitUI(self):
         #bbox = wx.BoxSizer(wx.VERTICAL)
         box = wx.GridBagSizer(10,10)
-        self.loginEntry = wx.TextCtrl(self.parent,value="",style=wx.wx.TE_LEFT|wx.TE_PROCESS_ENTER)
-        self.serveurEntry = wx.TextCtrl(self.parent,value="",style=wx.wx.TE_LEFT|wx.TE_PROCESS_ENTER)
+        self.loginEntry = wx.TextCtrl(self.parent,value="",
+                                      style=wx.wx.TE_LEFT|wx.TE_PROCESS_TAB)
+        self.serveurEntry = wx.TextCtrl(self.parent,value="",
+                                        style=wx.wx.TE_LEFT|wx.TE_PROCESS_ENTER|wx.TE_PROCESS_TAB)
         self.buttonEnter = wx.ToggleButton(self.parent,label="Entrer",id=wx.ID_ANY)
         self.buttonQuit = wx.Button(self.parent,label="Quitter",id=wx.ID_ANY)
+        # self.parent.Bind(wx.EVT_CHAR,self.onTabLogin,source=self.loginEntry)
+        # self.parent.Bind(wx.EVT_COMMAND_TEXT_ENTER,self.onEnterServeur,source=self.serveurEntry)
+        # self.parent.Bind(wx.EVT_CHAR,self.onTabServeur,source=self.serveurEntry)
         loginLabel = wx.StaticText(self.parent,label="Pseudo :")
         serveurLabel = wx.StaticText(self.parent,label="Adresse du serveur : ")
         spanTwoCol = wx.GBSpan(1,2)
@@ -48,7 +53,17 @@ class View(wx.BoxSizer):
         self.buttonEnter.Disable()
         event.Skip()
         #self.Close()
-        
+     
+    # def onEnterServeur(self,event):
+    #     self.buttonEnter.command(None)
+    
+    # def onTabLogin(self,event):
+    #     self.serveurEntry.GetFocus()
+
+    # def onTabServeur(self,event):
+    #     self.loginEntry.GetFocus()
+
+  
 if __name__ == '__main__':
     app = wx.App()
     View(None,title="Connection")
--- Client/python/main.py
+++ Client/python/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# main.py
 
 import wx
 import client
@@ -9,12 +10,18 @@ import connection
 class  Main(wx.App):
     
     def OnInit(self):
-        self.con = connection.Controler(None)
-        self.cli = client.Controler()
+        self.con = connection.Controler(self)
+        self.Bind(wx.EVT_TOGGLEBUTTON, self.onConnect,source=self.con.viou.buttonEnter)
+        self.Bind(wx.EVT_BUTTON, self.onQuit,source=self.con.viou.buttonQuit)
+        # self.cli = client.Controler()
         return True
 
     def onConnect(self,e):
-        print "coucou"
+        self.sock = self.con.sock
+        print str(self.sock.recv(4096))
+        
+    def onQuit(self,event):
+        self.ExitMainLoop()
 
 if __name__ == '__main__':
     app = Main()
