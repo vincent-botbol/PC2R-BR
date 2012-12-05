2f4ad3eac1585e86ad4c374a706e5100eaa34587
Matthieu Dien <matthieu@matthieu-Kubu.(none)>
2012-12-05 22:13:46 +0100

    python update

--- Client/python/client/controler.py
+++ Client/python/client/controler.py
@@ -5,6 +5,7 @@
 import view
 import socket
 import wx
+import re
 
 View = view.View
 
@@ -15,14 +16,27 @@ class Controler(wx.Frame):
         self.viou = View(self)
         self.Bind(wx.EVT_BUTTON,self.onBut)
         self.sock = sock
+
+        self.SetStatusBar(wx.StatusBar(self))
+        self.SetStatusText("En attente du serveur")
+
         self.Centre()
         self.Show()
 
-    #def putShip(self):
-        
+    def putShip(self):
+        for b in self.buts:
+            b.SetBitmapHover(self.subBmp)
+            b.Bind(wx.EVT_BUTTON,self.onBut)
+        rep = self.sock.recv(4096)
+        l = re.split("(?<!\\\)/",rep)
+        while l[O] <> 'ALLYOURBASE':
+            if l[O] == 'SHIP':
+                n = int(l[1])
+                self.SetStatusText("Placez un sous-marin de taille "+str(n))
+                pos = self.viou.putShip(n)
 
     def onBut(self,e):
-        print e.GetId()
+        print e.GetEventObject().GetName()
 
 if __name__ == '__main__':
     app = wx.App()
--- Client/python/client/view.py
+++ Client/python/client/view.py
@@ -6,11 +6,20 @@ import wx
 import chat
 import wx.lib.newevent
 import wx.lib.stattext
+import wx.lib.buttons
+
+#img = wx.Image("../img/sea.jpg")
+
 
 class View(wx.Panel):
     def __init__(self,parent):
         super(View, self).__init__(parent)
+        self.seaBmp = wx.Bitmap("../img/sea.jpg",wx.BITMAP_TYPE_JPEG)
+        self.subBmp =  wx.Bitmap("../img/submarin.jpeg",wx.BITMAP_TYPE_JPEG)
         self.buts = []
+        self.count = 0
+        self.pos = None
+
         bs=wx.BoxSizer(wx.HORIZONTAL)
         gs=wx.GridSizer(17,17,0,0)
         gs.AddStretchSpacer()
@@ -21,14 +30,21 @@ class View(wx.Panel):
             st = wx.lib.stattext.GenStaticText(self,wx.ID_ANY,str(15-i),style=wx.ALIGN_CENTRE)
             gs.Add(st,proportion=1,flag=wx.ALIGN_CENTRE)
             for j in range(16):
-                but = wx.Button(self,id=i,label='('+str(i)+','+str(j)+')')
-                but.Bind(wx.EVT_BUTTON,self.onBut)
+                but = wx.BitmapButton(self,wx.ID_ANY,self.seaBmp
+                                                     ,name=str(i)+','+str(j),style=wx.BORDER_NONE)
+                but.SetMargins(0,0)
+                #but = wx.StaticBitmap(self,wx.ID_ANY,self.bmp)
+                #but.Bind(wx.EVT_SET_FOCUS,self.onFocus)
                 self.buts.append(but)
                 gs.Add(but,proportion=1,flag=wx.EXPAND)
+
         bs.Add(gs,proportion=1,flag=wx.EXPAND|wx.LEFT)
         
         vbox=wx.BoxSizer(wx.VERTICAL)
+        vbox.AddStretchSpacer(prop=1)
+
         box = wx.BoxSizer(wx.HORIZONTAL)
+
         self.chatEntry = wx.TextCtrl(self,style=wx.TE_LEFT
                                      |wx.TE_PROCESS_ENTER)
         self.chatAll = wx.TextCtrl(self,style=wx.TE_MULTILINE
@@ -37,18 +53,31 @@ class View(wx.Panel):
         self.sendButton = wx.Button(self,id=wx.ID_OK)
         box.Add(self.chatEntry,proportion=1)
         box.Add(self.sendButton,proportion=1)
-        
+
         self.playerInfo = wx.StaticText(self,wx.ID_ANY,"Aucun joueur")
         
         vbox.Add(self.playerInfo,proportion=1,flag=wx.EXPAND|wx.CENTRE)
         vbox.Add(self.chatAll,proportion=1,flag=wx.EXPAND|wx.CENTRE)
         vbox.Add(box,proportion=0,flag=wx.EXPAND|wx.CENTRE)
+        vbox.AddStretchSpacer(prop=1)
 
-        
+
+        # self.Bind(wx.EVT_SIZE,self.onResize) 
+        # self.onResize(None)
+
         bs.Add(vbox,proportion=1,flag=wx.EXPAND|wx.RIGHT)
         self.SetSizer(bs)
+        
+        self.putShip(0)
+
         self.Show()
         # self.Bind(wx.EVT_KEY_DOWN,onPressKey)
+
+
+    def putShip(self,n):
+        self.count = 1
+        
+            
 
     # def onPressKey(self,event):
     #     win = FindFocus()
@@ -59,7 +88,16 @@ class View(wx.Panel):
     #             if kc = WXK_UP
                 
         
+    # def onResize(self,e):
+    #     (w,h) = self.buts[0].GetSizeTuple()
+    #     self.img.Rescale(w,h)
+    #     bmp = wx.BitmapFromImage(self.img)
+    #     for b in self.buts:
+    #         b.SetBitmapLabel(bmp)
 
+    # def onFocus(self,e):
+    #     print e.GetWindow().GetName()
 
     def onBut(self,e):
+        e.GetEventObject().SetBitmapLabel(self.subBmp)
         e.Skip()
